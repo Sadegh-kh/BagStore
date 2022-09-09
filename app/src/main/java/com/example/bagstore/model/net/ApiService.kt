4373dad8dcca48dd71598dc191d60e3793a1abb2
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-09-09 14:18:19 +0430

    init api product and advertisement request

--- app/src/main/java/com/example/bagstore/model/net/ApiService.kt
+++ app/src/main/java/com/example/bagstore/model/net/ApiService.kt
@@ -1,6 +1,8 @@
 package com.example.bagstore.model.net
 
+import com.example.bagstore.model.data.AdvertisementsResponse
 import com.example.bagstore.model.data.LoginResponse
+import com.example.bagstore.model.data.ProductResponse
 import com.example.bagstore.model.repository.TokenInMemory
 import com.google.gson.JsonObject
 import retrofit2.Call
@@ -11,12 +13,24 @@ import retrofit2.http.POST
 
 interface ApiService {
 
+    //sign in and sing up screen api
     @POST("signUp")
     suspend fun singUp(@Body jsonObject: JsonObject):LoginResponse
 
     @POST("signIn")
     suspend fun singIn(@Body jsonObject: JsonObject):LoginResponse
 
+
     @GET("refreshToken")
     fun refreshToken():Call<LoginResponse>
+
+
+    //main screen , product and advertisement api
+    @GET("getProducts")
+    suspend fun getAllProducts():ProductResponse
+
+    @GET("getSliderPics")
+    suspend fun getAllAdvertisements():AdvertisementsResponse
+
+
 }
