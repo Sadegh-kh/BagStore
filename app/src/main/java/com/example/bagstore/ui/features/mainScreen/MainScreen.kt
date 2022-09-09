24658245d50155379aa7d1ae1d03074ec0c21e0b
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-09-09 09:21:39 +0430

    init list of product demo

--- app/src/main/java/com/example/bagstore/ui/features/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/bagstore/ui/features/mainScreen/MainScreen.kt
@@ -8,17 +8,16 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.ShoppingBag
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bagstore.R
 import com.example.bagstore.ui.theme.BackgroundBlue
 import com.example.bagstore.ui.theme.BackgroundMain
@@ -31,8 +30,76 @@ fun MainScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
         TopToolBar()
         CategoryList()
+
+        ProductSubject()
     }
 
+}
+
+@Composable
+fun ProductSubject() {
+    Column {
+        Text(
+            text = "Popular Destinations",
+            style = MaterialTheme.typography.h6,
+            fontSize = 25.sp,
+            modifier = Modifier.padding(top = 30.dp, bottom = 20.dp, start = 20.dp)
+        )
+        LazyRow(contentPadding = PaddingValues(end = 20.dp)) {
+            items(10) {
+                ProductItem()
+            }
+        }
+
+    }
+}
+
+@Composable
+fun ProductItem() {
+    Card(modifier = Modifier
+        .wrapContentSize()
+        .padding(start = 20.dp)
+        .clickable {  },
+        shape = Shapes.medium) {
+        Column(modifier = Modifier.wrapContentSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.img_intro),
+                contentDescription = "Product Subject Image",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth().size(250.dp)
+            )
+
+            ProductTextBox()
+
+        }
+
+
+    }
+}
+
+@Composable
+fun ProductTextBox() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(250.dp,100.dp)
+            .background(Color.White), contentAlignment = Alignment.TopStart
+    ) {
+        Column(Modifier.padding(10.dp)) {
+            Text(
+                text = "Diamond Woman Watches",
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier.padding(bottom = 5.dp),
+                fontSize = 18.sp,
+                maxLines = 1
+            )
+
+            Text(text = "86,000 Tomans", fontSize = 17.sp, color = Color.DarkGray)
+
+            Text(text = "156 sold", fontSize = 15.sp, color = Color.Gray)
+        }
+
+    }
 }
 
 @Composable
@@ -58,7 +125,7 @@ fun CategoryItem() {
                 contentDescription = "categoryItem",
                 modifier = Modifier
                     .background(BackgroundBlue)
-                    .clickable {  }
+                    .clickable { }
                     .padding(18.dp)
             )
 
