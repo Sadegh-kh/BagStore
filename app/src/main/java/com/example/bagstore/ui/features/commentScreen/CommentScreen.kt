d0220daf8669cc0751812d3187d50bb3919115e7
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-10-04 14:51:41 +0330

    init scroll behavior for top bar and show list of comment demo

--- app/src/main/java/com/example/bagstore/ui/features/commentScreen/CommentScreen.kt
+++ app/src/main/java/com/example/bagstore/ui/features/commentScreen/CommentScreen.kt
@@ -4,6 +4,9 @@ package com.example.bagstore.ui.features.commentScreen
 
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIos
 import androidx.compose.material.icons.filled.ArrowBackIosNew
@@ -11,29 +14,55 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.bagstore.model.data.Comment
+import com.example.bagstore.ui.features.productScreen.CommentItem
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @ExperimentalMaterial3Api
 @Composable
 fun CommentScreen(productId: String) {
-    Scaffold(topBar = {
-        CenterAlignedTopAppBar(
-            modifier = Modifier.shadow(5.dp),
-            title = {
-                Text(text = "Comments", fontSize = 20.sp)
-            },
-            navigationIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Default.ArrowBackIosNew, contentDescription = "onBack")
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CenterAlignedTopAppBar(
+                modifier = Modifier.shadow(5.dp),
+                title = {
+                    Text(text = "Comments", fontSize = 20.sp)
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBackIosNew,
+                            contentDescription = "onBack"
+                        )
 
-                }
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+                //color when scrolled
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    scrolledContainerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
+                        0.dp
+                    )
+                )
+
+            )
+        }) {
+
+        LazyColumn(modifier = Modifier.padding(horizontal = 10.dp)){
+            item {
+                Spacer(modifier = Modifier.padding(top = 80.dp))
             }
-
-        )
-    }) {
+            items(10){
+                CommentItem(comment = Comment("2","Very Good","SadeghKHoshbaya@gmail.com"))
+            }
+        }
 
 
     }
